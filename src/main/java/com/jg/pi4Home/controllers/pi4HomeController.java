f296bd5c560713b5a4f039905d10b2aaa039b9f9
Josef Grinspun <josef.grinspun@bitingbit.com>
2018-04-14 14:32:46 +0200

    added the basic endpoints for openening and closing the shutter

--- src/main/java/com/jg/pi4Home/controllers/pi4HomeController.java
+++ src/main/java/com/jg/pi4Home/controllers/pi4HomeController.java
@@ -13,4 +13,19 @@ public class pi4HomeController {
     public String entryPoint() {
         return "You are on my pi4Home server";
     }
+
+    @RequestMapping("/open")
+    public String openShutter() {
+        return "You are now opening the shutter;";
+    }
+
+    @RequestMapping("close")
+    public String closeShutter() {
+        return "You are now closing the shutter!";
+    }
+
+    @RequestMapping("stop")
+    public String stopMovingShutter() {
+        return "You have stopped the shutter from moving further!";
+    }
 }
