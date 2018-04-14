224e67a0750dc452079f338fef28032289c1345f
Josef Grinspun <josef.grinspun@bitingbit.com>
2018-04-14 13:33:31 +0200

    init commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by josefgrinspun on 14.04.1l8.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
